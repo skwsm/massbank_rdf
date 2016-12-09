636d77924c54d9875a2083cf1e5e0e5e3c0f74f1
Shuichi Kawashima <shuichi.kawashima@gmail.com>
2016-12-09 11:35:49 +0900

    Commit bin directory and massbank2rdf.rb

---
+++ bin/massbank2rdf.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'rdf'
+require 'rdf/ntriples'
+require 'rdf/turtle'
+require './massbank.rb'
+include RDF
+
+f = open(ARGV.shift)
+f_out = open(ARGV.shift, "w")
+r = MassBank::Record.new(f.read)
+#r.record.each do |e|
+#  p e
+#end
+factory = MassBank::RDFFactory.new(r.record, f_out)
+factory.rdfize
+
